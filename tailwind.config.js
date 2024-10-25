c99878eb18b55338b575deb9b2c6c81692fab17d
crvargasm <crvargasm@unal.edu.co>
2024-10-24 20:43:15 -0500

    retoma proceso innovaTE

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,4 +2,12 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   prefix: 'tw-',
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Montserrat', 'sans-serif'],
+        indie: ['IndieFlower', 'cursive'],
+      },
+    },
+  },
 }
